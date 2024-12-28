caeeb111c7e4743f29375c4e04059492d61c0372
1baggboy <lookuptoadams@gmail.com>
2024-12-28 12:03:32 +0100

    Contact modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Contact  from './pages/contact'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+    <Contact/>
     </>
   )
 }
 
-export default App
+export default App;
