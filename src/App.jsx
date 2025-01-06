dbbd5f39914b5f3ec3f19332fb8963a59a7fa1c9
1baggboy <121406154+1baggboy@users.noreply.github.com>
2025-01-06 12:34:37 +0100

    App.jsx is being implimented

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-
-    </>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+    </Routes>
   )
 }
 
