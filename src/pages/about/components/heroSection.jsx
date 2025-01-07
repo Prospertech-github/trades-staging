51495cbdc4a15049177dadfa78101746a57dabb8
Prospertech-github <ikechukwuprosper8@gmail.com>
2025-01-07 15:17:25 +0100

    FEAT: Started Developing the About Page

---
+++ src/pages/about/components/heroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import hero from "./heroSection.module.css"
+
+function HeroSection() {
+  return (
+    <div className={hero.heroDiv}>
+        <h2>About</h2>
+    </div>
+  )
+}
+
+export default HeroSection
