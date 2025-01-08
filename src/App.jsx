cecada85c90b87f7a4ee2f056fb1d1705f35b7ed
1baggboy <121406154+1baggboy@users.noreply.github.com>
2025-01-08 02:47:08 +0100

    Dev/Alex implimented changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
+import './App.css'
+import Button from './uiComponents/Button/Button';
+import ContactForm from './uiComponents/ContactForm/ContactForm';
+import ContactHeader from './uiComponents/ContactHeader/ContactHeader';
+import Navigation from "./uiComponents/Navigation/Navigation";
+
 
 function App() {
   const [count, setCount] = useState(0)
-
+  
   return (
+  <div className='container'>
+    <Navigation />
+    <ContactHeader />
+    <ContactForm />
+    
     <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/about' element={<About />}/>
     </Routes>
+  </div> 
   )
 }
 
