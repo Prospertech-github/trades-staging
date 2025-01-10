2d2a7f5c268b0dc59047f57fc706ab4028c33a2b
Prospertech-github <ikechukwuprosper8@gmail.com>
2025-01-10 12:11:00 +0100

    FEAT: Completed the About page for the Dekstop screen

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Outlet from '../../uiComponents/outlet'
 import HeroSection from './components/heroSection'
 import WhySection from '../home/components/whySection/whySection'
+import HowtoTrade from './components/howtoTrade'
 import about from './about.module.css'
 
 function About() {
@@ -11,6 +12,7 @@ function About() {
       <div className={about.whyDiv}>
         <WhySection />
       </div>
+      <HowtoTrade />
     </Outlet>
   )
 }
