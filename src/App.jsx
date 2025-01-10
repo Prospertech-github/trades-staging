5dd34359fb0683920ee28cd7918913a2ba68260b
Prospertech-github <ikechukwuprosper8@gmail.com>
2025-01-10 13:32:10 +0100

    FEAT: Completed Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import {Routes, Route} from 'react-router-dom'
 import Home from './pages/home'
 import About from './pages/about'
-import Services from './pages/services'
+import Login from './pages/login'
 
 function App() {
 
@@ -9,7 +9,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/about' element={<About />}/>
-      <Route path='/services' element={<Services />} />
+      <Route path='/login' element={<Login />}/>
     </Routes>
   )
 }
